5c75b5fbd7ef0567f5917c756a06a42439996372
Rich Korneisel <korneisel@cfu.net>
2021-03-01 09:51:19 -0600

    added link, title takes back to home page

--- charging-locator-app/src/components/Header/Header.js
+++ charging-locator-app/src/components/Header/Header.js
@@ -1,10 +1,14 @@
+import {Link} from 'react-router-dom';
 import './Header.css';
 import Nav from '../Nav/Nav'
+
 
 function Header() {
     return (
       <div>
-        <h1 className='appName'>Charging Station Locator App</h1>
+        <Link to = '/'>
+          <h1 className='appName'>Charging Station Locator App</h1>
+        </Link>
         <Nav />
       </div>
     );
